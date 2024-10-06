8cf0fe05d4f29320a176d4d41ac8354f539fe679
Kimyechan2154 <kimyechan2959@gmail.com>
2024-10-06 19:49:56 +0900

    Add files via upload

---
+++ for.c
@@ -0,0 +1,107 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+
+int main(){
+    
+    int i;
+    
+    printf("hi \n");
+    
+    goto end;
+    
+    printf("bye \n");
+
+    printf("hello \n");
+end:
+    printf("!!end!! \n");
+
+    
+    return 0;
+}
+
+// int main(){
+    
+//     int i;
+    
+//     for(i=0; i<10; i++){
+//         if(i%2 ==0){
+//             continue;
+//         }
+//         printf("Hello World %d\n", i);
+//     }
+//     return 0;
+// }
+
+// int main(){
+    
+//     int i;
+    
+//     srand(time(NULL));    
+    
+//   while(1){
+//         int rnd = rand()%1000+1;
+//   printf("%d\n" ,rnd);
+//   if(rnd==555){
+//       break;
+//   }
+//   }
+//     return 0;
+// }
+
+// int main(){
+   
+//   int i;
+
+//   int j;
+   
+//   for(i=0; i<10; i++){
+//       for(j=0; j<10; j++){
+//           printf("%d %d\n", i, j);
+//       }
+//   }
+   
+//     return 0;
+// }
+
+// int main(){
+   
+//   int i = 0;
+   
+//   while(i<10)
+// {
+//      printf("Hello wrold %d\n" , i);
+//      i++;
+// }}
+
+
+// int main(){
+   
+//   int i;
+   
+//   for(i=-5; i<1; i++){
+//  printf("Hello wrold %d\n" , i);
+//   }
+//   return 0;
+// }
+
+
+
+
+
+//   char a[10] ={"hello"};
+   
+   
+   
+//   printf("당신이 제일 좋아하는 숫자는 무엇입니까");
+   
+//   scanf("%f", &box);
+   
+//   printf("\n 당신의 키는 %.2f 입니다", box);
