2b014414768af27f3054c8615e1944aaade67f6d
Kimyechan2154 <kimyechan2959@gmail.com>
2024-10-10 23:13:20 +0900

    Add files via upload

---
+++ 10.10.c
@@ -0,0 +1,61 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+
+#define _crt_secure_no_warnings
+#include <stdio.h>
+
+int main(){
+    
+    int n, i, j;
+    
+    scanf("%d", &n);
+    
+    
+    for(i=n; i>0; i--){
+       for(j=1; j<i+1; j++){
+           printf("*");
+       }
+    printf("\n");
+     
+    }
+    
+    return 0;
+}
+
+
+// int main(){
+    
+//     int a, b, c, i, sum[3]={0,0,0};
+    
+//     scanf("%d %d", &a, &b);
+    
+//     c = b + 1;
+    
+   
+//     for (; a<c; a++)
+//     {
+        
+//     i = a % 2;
+    
+//     if(i == 0){
+//         sum[0]-= a;
+//     }
+    
+//     else if(i == 1){
+//         sum[1] += a; 
+//     } 
+//   sum[2] = sum[0] + sum[1];
+//     }
+
+    
+//     printf("%d", sum[2]);
+    
+//     return 0;
+    
+// }
