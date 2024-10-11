acbf18de0182954f8c2cc8b3fc8c013c658225f6
Kimyechan2154 <kimyechan2959@gmail.com>
2024-10-11 18:20:39 +0900

    Add files via upload

---
+++ 1011(1).c
@@ -0,0 +1,94 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+#define _crt_secure_no_warnings
+#include <stdio.h>
+
+// int main(){
+    
+//     int n;
+    
+//     char l[5]= "love";
+    
+//     char o[n] = { 0 };
+    
+//     scanf("%c",);
+    
+//     printf("%c", l[1]);
+    
+    
+// }
+
+
+// int main(){
+    
+//     int num[10] = { 0 }, i, j;
+    
+//     for(j=0; j<10; j++){
+//     scanf("%d", &num[j]);
+//     }
+    
+//     scanf("%d", &i);
+    
+//     printf("%d", num[i-1]);
+    
+//     return 0;
+// }
+
+
+// int main(){
+    
+//     int n, i, j;
+    
+//     scanf("%d", &n);
+    
+    
+//     for(i=n; i>0; i--){
+//       for(j=1; j<i+1; j++){
+//           printf("*");
+//       }
+//     printf("\n");
+     
+//     }
+    
+//     return 0;
+// }
+
+
+// int main(){
+    
+//     int a, b, c, i, sum[3]={0,0,0};
+    
+//     scanf("%d %d", &a, &b);
+    
+//     c = b + 1;
+    
+   
+//     for (; a<c; a++)
+//     {
+        
+//     i = a % 2;
+    
+//     if(i == 0){
+//         sum[0]-= a;
+//     }
+    
+//     else if(i == 1){
+//         sum[1] += a; 
+//     } 
+//   sum[2] = sum[0] + sum[1];
+//     }
+
+    
+//     printf("%d", sum[2]);
+    
+//     return 0;
+    
+// }
