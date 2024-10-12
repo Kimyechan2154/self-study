86104abce4fd7492550fd14e235ea610bf88ee95
Kimyechan2154 <kimyechan2959@gmail.com>
2024-10-12 19:55:00 +0900

    Add files via upload

---
+++ 10.12(2).c
@@ -0,0 +1,81 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#define _crt_secure_no_warnings
+#include <stdio.h>
+
+int main(){
+    int n, i, j, a[101], v, k = 0;
+    
+    scanf("%d", &n);
+
+    for(i=0; i<n; i++){
+    scanf("%d", &a[i]);
+    
+    } 
+    
+    scanf("%d", &v);
+
+    
+    for(j=0; j<n; j++){
+        if(v == a[j]){
+            k += 1;
+        }
+    }
+    
+    printf("%d",k);
+    return 0;
+}
+
+
+
+// int main(){
+//     int i, n, j;
+    
+//     scanf("%d", &i);
+    
+//     int a[i], b[i], c[i];
+    
+//     for(n=0; n<i; n++){
+//         scanf("%d %d", &a[n], &b[n]);
+        
+//         c[n] = a[n] + b[n];
+        
+        
+//     }
+    
+//      for(j=0; j<i; j++){
+//         printf("Case #%d: %d\n",j+1, a[j]+b[j]);
+//      }
+// }
+
+
+
+// int main() {
+
+// 	int x, n, i, j = 0, a[100] = {0}, b[100] = {0};
+
+// 	scanf("%d %d", &x, &n);
+
+
+// 	for (i=0; i<n; i++) {
+// 		scanf("%d %d", &a[n], &b[n]);
+// 		j += a[n] * b[n];
+
+
+// 	}
+	
+// 	if(x == j){
+// 	    printf("Yes");
+// 	}
+	
+// 	else{
+// 	    printf("No");
+// 	}
+// 	return 0;
+// }
