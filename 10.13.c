2dced160ff3ff39730bab60cc23bf4b0543d8206
Kimyechan2154 <kimyechan2959@gmail.com>
2024-10-13 19:57:50 +0900

    Add files via upload

---
+++ 10.13.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+
+    int m, n;
+    
+    scanf("%d %d", &n, &m);
+    
+    int a[n], b[n];
+    
+
+    
+    return 0;
+}
+
+// int main() {
+
+//     int a[9] = {0}, i, j=0, k=0;
+
+//     for(i=0; i<9; i++){
+//     scanf("%d", &a[i]);
+
+//     if(j<a[i]){
+//             j = a[i];
+//             k = i;
+//         }
+//     }
+
+//     printf("%d %d", j, k+1);
+        
+//     return 0;
+// }
