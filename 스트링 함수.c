8e29052d464f0894bceb0f082954c0efdc2035ea
Kimyechan2154 <kimyechan2959@gmail.com>
2024-10-15 22:37:23 +0900

    Add files via upload

---
+++ 스트링 함수.c
@@ -0,0 +1,74 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#define _crt_secure_no_warnings
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+    
+    char ch[100] = "chani";
+    
+ strlen   
+ return 0;
+}
+
+
+// int main(){
+    
+//     char ch[100] = "chani";
+    
+//     printf("\n %d \n", ch);
+//     printf("\n %d \n", &ch[0]);
+//     printf("\n %d \n", *&ch);
+    
+//     *arr == a[0];
+//     *(arr+1) == a[1];
+    
+//     return 0;
+// }
+
+// int main(){
+    
+//  int n;
+//  scanf("%d", &n);
+ 
+ 
+//  for(int j = 0; j<n.size(); j++){
+//  scanf("%d", &a[j]);
+
+//  }
+ 
+//  printf("%d", a[3]);
+//     return 0;
+
+// }
+
+// int main(){
+    
+//     char str[1001];
+//     int i;
+//     scanf("%s", str);
+//     scanf("%d", &i);
+    
+//     printf("%c", str[i-1]);
+    
+//     return 0;
+
+// }
+
+// int main(){
+    
+//     char str;
+//     scanf("%c", &str);
+    
+//     printf("%d", str);
+    
+//     return 0;
+
+// }
